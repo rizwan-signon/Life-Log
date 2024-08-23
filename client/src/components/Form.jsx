8622ed4fbda2302501945153c6f9244ac098c8ab
your-username <your-email>
2024-08-23 22:38:43 +0500

    add daisy ui

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,10 @@
+const Form = () => {
+  return (
+    <div>
+      <h1>form</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+};
+
+export default Form;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="max-w-full bg-primary">
+      <div className=" max-w-6xl mx-auto flex items-center justify-between">
+        <h1 className="text-4xl text- font-medium">LiFeLog</h1>
+        <h2>image</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
