1d090764e8779fed25f5748434311806753f041c
your-username <your-email>
2024-08-24 02:59:54 +0500

    add update route

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Post from "../models/posts.model.js";
 export const getPosts = async (req, res, next) => {
   try {
@@ -14,6 +15,21 @@ export const createPost = async (req, res, next) => {
     const newPost = await new Post(postBody);
     await newPost.save();
     res.status(200).json(newPost);
+  } catch (error) {
+    next(error.message);
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  const { id: _id } = req.params.id;
+  const postBody = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id))
+      throw new Error("the id provided is not correct format");
+    const updatedPost = await Post.findByIdAndUpdate(_id, postBody, {
+      new: true,
+    });
+    res.status(200).json(updatePost);
   } catch (error) {
     next(error.message);
   }
--- server/routes/posts.route.js
+++ server/routes/posts.route.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { getPosts, createPost } from "../controllers/posts.controllers.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+} from "../controllers/posts.controllers.js";
 const router = Router();
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
 export default router;
