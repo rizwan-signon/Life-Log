06c3c31ec3c5f5791b02375968cd1b6d51678966
your-username <your-email>
2024-08-24 00:22:19 +0500

    modify form to reacieve tags

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,17 +1,82 @@
 import { useState } from "react";
 import FileBase from "react-file-base64";
 const Form = () => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: [],
+  });
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+
+    if (id === "tags") {
+      const tagsArray = value
+        .split(",")
+        .map((tag) => tag.trim())
+        .filter((tag) => tag.length > 0);
+      setFormData({ ...formData, [id]: tagsArray });
+    } else {
+      setFormData({ ...formData, [id]: value });
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:3000/posts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  console.log(formData);
   return (
-    <form className="bg-blue-600 w-1/3">
-      <h1>lets create your memory</h1>
-      <div className="">
-        <input type="text" placeholder="creator/owenor" id="creator" />
-        <input type="text" placeholder="title" id="title" />
-        <input type="text" placeholder="message" id="message" />
-        <input type="text" placeholder="tags" id="tags" />
+    <form
+      onSubmit={handleSubmit}
+      className="bg-blue-600 w-1/3 p-4 flex flex-col gap-6"
+    >
+      <h1 className="text-center text-2xl font-bold capitalize  ">
+        lets create your memory
+      </h1>
+      <div className="w-full mx-auto flex flex-col gap-4">
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="creator/owenor"
+          id="creator"
+          className=" p-3 outline-none bg-gray-300 rounded-md"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="title"
+          id="title"
+          className=" p-3 outline-none bg-gray-300 rounded-md"
+        />
+
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="message"
+          id="message"
+          className=" p-3 outline-none bg-gray-300 rounded-md"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="tags"
+          id="tags"
+          className=" p-3 outline-none bg-gray-300 rounded-md"
+        />
       </div>
-      <div>
+      <div className=" border-2 p-2 rounded-lg">
         <FileBase
           type="file"
           multiple={false}
@@ -19,6 +84,17 @@ const Form = () => {
             setFormData({ ...formData, selectedFile: base64 })
           }
         />
+      </div>
+      <div className="flex flex-col gap-5">
+        <button
+          type="submit"
+          className="p-3 uppercase text-xl bg-green-700 rounded-lg hover:opacity-85"
+        >
+          Submit
+        </button>
+        <button className="p-3 uppercase text-xl bg-red-700 rounded-lg hover:opacity-85">
+          clear
+        </button>
       </div>
     </form>
   );
--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -9,9 +9,9 @@ export const getPosts = async (req, res, next) => {
 };
 
 export const createPost = async (req, res, next) => {
-  const { title } = req.body;
+  const postBody = req.body;
   try {
-    const newPost = await new Post({ title });
+    const newPost = await new Post(postBody);
     await newPost.save();
     res.status(200).json(newPost);
   } catch (error) {
