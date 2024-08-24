a557754f3fa4818eed4d06b87073f3571826d9ed
your-username <your-email>
2024-08-25 01:33:47 +0500

    update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div className="w-full rounded-lg bg-gray-800">
       <div className=" sm:max-w-4xl max-w-lg mx-auto flex items-center sm:justify-between justify-around py-2">
-        <h1 className="text-4xl text- font-medium capitalize">LiFLog</h1>
+        <h1 className="text-4xl text- font-medium capitalize">LiFELog</h1>
         <img src={logo} alt="logo" className="min-w-12 min-h-12 rounded-full" />
       </div>
     </div>
